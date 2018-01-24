b7260a1971e2cbf8727979010f6686d39e817f9f
hewang <hewang2@cisco.com>
2018-01-24 14:10:26 +0800

    用于测试的code

---
+++ test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# 用于测试code的文件
+L = [1,2,3,4]
+if type(L) == type([]):
+	print("L is list")
+if type(L) == list:
+	print("L is list")
+if isinstance(L,list):
+	print("L is list")
