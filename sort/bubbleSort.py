799264fb5e8ab07d093ed0c00668fef68db0ad8e
wanghe <yueyesuifeng@sina.com>
2018-02-06 23:25:42 +0800

    冒泡排序

---
+++ sort/bubbleSort.py
@@ -0,0 +1,11 @@
+def bubbleSort(arr):
+    for i in range(1, len(arr)):
+        for j in range(0, len(arr) - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+
+print "hello"
+x = bubbleSort([3, 1, 4, 3, 2, 7])
+print x
